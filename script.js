be647bd22815b988e8e138f474949118061c5535
André Gabriel B. L. <andregabrielbarbosa@gmail.com>
2026-05-07 15:02:52 -0300

    Adiciona Accordion List.

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
-if(tabMenu.length && tabContent.length) {
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  const ativo = "ativo";
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add(ativo);
 
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove(ativo);
+      });
+      tabContent[index].classList.add(ativo);
+    }
 
-tabContent[0].classList.add("ativo");
-
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove("ativo");
-  });
-  tabContent[index].classList.add("ativo");
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
 }
+initTabNav();
 
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener("click", () => {
-    activeTab(index);
-  });
-});
+function initAccorddion() {
+  const accorddionList = document.querySelectorAll(".js-accorddion dt");
+  const ativo = "ativo";
+  if (accorddionList.length) {
+    accorddionList[0].classList.add(ativo);
+    accorddionList[0].nextElementSibling.classList.add(ativo);
+
+    accorddionList.forEach((item) => {
+      item.addEventListener("click", activeAccorddion);
+    });
+
+    function activeAccorddion() {
+      this.classList.toggle(ativo);
+      this.nextElementSibling.classList.toggle(ativo);
+    }
+  }
 }
+initAccorddion();
